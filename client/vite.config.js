229570fc4c37874d527dfa8599e90fc12973ea3e
Alexandru Doni <donialexandru@gmail.com>
2025-11-20 17:57:19 +0200

    add vite proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    "/api": {
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    },
+    "/public": {
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    },
+  }
   plugins: [react()],
 });
