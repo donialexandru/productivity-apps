497f9a9a2c0dd82e2bdc8e39042a4346de304e8c
Alexandru Doni <donialexandru@gmail.com>
2026-01-28 14:29:09 +0200

    add the default middlewares

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,22 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import counterRoutes from './routes/counterRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTestEnv } from '../env.ts'
 
 const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTestEnv(),
+  }),
+)
 
 // Health check enpoint
 app.get('/health', (req, res) => {
