23d899675b69e3ab38f4ed35e2570d16cab00503
Alexandru Doni <donialexandru@gmail.com>
2026-01-28 15:41:17 +0200

    added the middleware

--- server/src/routes/counterRoutes.ts
+++ server/src/routes/counterRoutes.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express'
+import { validateBody } from '../midlleware/validation.ts'
+import { z } from 'zod'
+
+const createCounterSchema = z.object({
+  name: z.string(),
+})
+
+const createParamsSchema = z.object({
+  id: z.string().min(1),
+})
 
 const router = Router()
 
-router.post('/create', (req, res) => {
+router.post('/', validateBody(createCounterSchema), (req, res) => {
   res.status(201).json({ message: 'counter successfully created' })
 })
 
