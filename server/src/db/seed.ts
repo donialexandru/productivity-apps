9e7926347c68adc3a3b512230945e352b7865570
Alexandru Doni <donialexandru@gmail.com>
2026-01-29 18:33:24 +0200

    add the seed script

---
+++ server/src/db/seed.ts
@@ -0,0 +1,49 @@
+import { db } from './connection.ts'
+import { users, counters } from './schema.ts'
+
+const seed = async () => {
+  console.log('🌱 Starting database seed...')
+
+  try {
+    console.log('Clearign existing data....')
+    await db.delete(users)
+    await db.delete(counters)
+
+    console.log('Creating demo users...')
+    const [demoUser] = await db
+      .insert(users)
+      .values({
+        email: 'demo@app.com',
+        password: 'password',
+        firstName: 'demo',
+        lastName: 'person',
+        username: 'demo',
+      })
+      .returning()
+
+    console.log('Creating counters...')
+    const [ticketCounter] = await db
+      .insert(counters)
+      .values({
+        userId: demoUser.id,
+        name: 'Tickets',
+        description: 'A simple counter for tickets',
+      })
+      .returning()
+
+    console.log('✅ DB seeded successfully')
+    console.log('user credentials:')
+    console.log(`email: ${demoUser.email}`)
+    console.log(`username: ${demoUser.username}`)
+    console.log(`password: ${demoUser.password}`)
+  } catch (e) {
+    console.log('😩 seed failed', e)
+    process.exit(1)
+  }
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  seed()
+    .then(() => process.exit(0))
+    .catch((e) => process.exit(1))
+}
