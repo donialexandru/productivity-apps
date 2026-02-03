56b43f26cf3699b4bf72f45640fe868b1479e127
Alexandru Doni <donialexandru@gmail.com>
2026-02-03 11:40:51 +0200

    use counter controller to get all counters from database

--- server/src/routes/countersRoutes.ts
+++ server/src/routes/countersRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { validateBody } from '../midlleware/validation.ts'
 import { z } from 'zod'
+import { getUserCounters } from '../controllers/counterController.ts'
 
 const createCounterSchema = z.object({
   name: z.string(),
@@ -12,27 +13,7 @@ const createParamsSchema = z.object({
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'All counters successfully retrieved',
-    counters: [
-      {
-        id: 1,
-        name: 'My first counter',
-        description: 'This counter was created for the first time',
-        currentCount: 0,
-        targetCount: 10,
-      },
-      {
-        id: 2,
-        name: 'Second counter',
-        description: 'This counter count to 5',
-        currentCount: 2,
-        targetCount: 5,
-      },
-    ],
-  })
-})
+router.get('/', getUserCounters)
 
 router.post('/', validateBody(createCounterSchema), (req, res) => {
   res.status(201).json({ message: 'counter successfully created' })
