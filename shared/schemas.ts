39d6f1f7c91ceb3b7fdc2e4ae48a7f81cac46e39
Alexandru Doni <donialexandru@gmail.com>
2026-02-14 19:10:03 +0200

    update schemas

--- shared/schemas.ts
+++ shared/schemas.ts
@@ -11,9 +11,18 @@ export const CounterSchema = z.object({
   name: z.string(),
   currentCount: z.number(),
   targetCount: z.number(),
-  createdAt: z.string(),
-  updatedAt: z.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
 });
+
+export const CreateCounterSchema = CounterSchema.pick({
+  name: true,
+  targetCount: true,
+});
+export type CreateCounterInput = z.infer<typeof CreateCounterSchema>;
+
+export const UpdateCounterSchema = CounterSchema.partial().omit({ id: true });
+export const DeleteCounterSchema = CounterSchema.pick({ id: true });
 
 export const CounterSchemaArray = z.array(CounterSchema);
 
