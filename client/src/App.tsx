7ab4fe4d3716501e6dbf89e6b759532c4a7d9d98
Alexandru Doni <donialexandru@gmail.com>
2026-03-04 22:22:44 +0200

    add a new version of React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useContext } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
