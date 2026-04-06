428aae9c9c9a03b4e358fde783e2e248e3bd92c1
Alexandru Doni <donialexandru@gmail.com>
2026-04-06 21:06:29 +0300

    update the auth schema

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: ({ context }) => {
-    if (context.isAuthenticated) {
+    if (context.isAuthenticated()) {
       throw redirect({ to: "/dashboard" });
     } else {
       throw redirect({ to: "/login" });
--- shared/src/users/users.schema.ts
+++ shared/src/users/users.schema.ts
@@ -3,7 +3,6 @@ import z from "zod";
 export const userSchema = z.object({
   id: z.uuid(),
   email: z.email(),
-  name: z.string(),
 });
 
 export type User = z.infer<typeof userSchema>;
