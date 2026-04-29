d6cca6e129260d4acd40ac4ed5279f4937490364
Alexandru Doni <donialexandru@gmail.com>
2026-04-29 21:08:49 +0300

    update the svg logo

--- client/src/components/layout/icons/Logo.tsx
+++ client/src/components/layout/icons/Logo.tsx
@@ -3,10 +3,9 @@ const MindFlowLogo = () => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 100 100"
-      width="200"
-      height="200"
+      width="40"
+      height="40"
     >
-      <rect width="100" height="100" fill="white" />
       <circle cx="50" cy="50" r="46" fill="black" />
       <circle cx="50" cy="50" r="38" fill="white" />
       <polygon points="57,12 36,52 50,52 43,88 68,44 53,44" fill="black" />
